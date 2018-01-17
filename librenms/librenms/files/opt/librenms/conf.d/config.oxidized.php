ade32fa5ca5aae43eccc0223b151971381d4062e
Daniel Olynsma <daniel.olynsma@alliedtelesis.co.nz>
2018-01-17 15:21:20 +1300

    librenms: add some oxidized options
    - Remove private IP addresses

--- librenms/librenms/files/opt/librenms/conf.d/config.oxidized.php
+++ librenms/librenms/files/opt/librenms/conf.d/config.oxidized.php
@@ -1,14 +1,17 @@
 <?php
 
-$config['oxidized']['enabled']         = TRUE;
-$config['oxidized']['url']             = 'http://127.0.0.1:8888';
+$config['oxidized']['enabled']         = true;
+$config['oxidized']['url']             = 'http://oxidized:8888';
+$config['oxidized']['features']['versioning'] = true;
 
 $config['oxidized']['group_support'] = true;
 $config['oxidized']['default_group'] = 'default';
+
+$config['oxidized']['ignore_types'] = array('allied');
+$config['oxidized']['ignore_os'] = array('linux');
 
 ### NOTE: Requires: $config['enable_syslog'] = 1;
 #save config when "wr" performed on device and log sent to syslog
 # $config['enable_syslog_hooks'] = 1;
 # $config['os']['ios']['syslog_hook'][] = Array('regex' => '/%SYS-(SW[0-9]+-)?5-CONFIG_I/', 'script' => '/opt/librenms/scripts/syslog-notify-oxidized.php');
 # $config['os']['awplus']['syslog_hook'][] = Array('regex' => '/\]wr/', 'script' => '/opt/librenms/scripts/syslog-notify-oxidized.php');
-
