d99e1f5f7b330c1816b6b45164bf22785f7ea8bb
Daniel Olynsma <daniel.olynsma@alliedtelesis.co.nz>
2018-01-23 10:05:44 +1300

    librenms: Add config.autodiscover_nets_php

---
+++ librenms/librenms/files/opt/librenms/conf.d/config.autodiscover_nets.php
@@ -0,0 +1,6 @@
+<?php
+
+### List of RFC1918 networks to allow scanning-based discovery
+#$config['nets'][] = "10.0.0.0/8";
+#$config['nets'][] = "172.16.0.0/12";
+#$config['nets'][] = "192.168.0.0/16";
