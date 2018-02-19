d42f8b8f48def4b914d88ed90e6c9714b1c509e6
Daniel Olynsma <daniel.olynsma@alliedtelesis.co.nz>
2018-02-19 13:16:53 +1300

    librenms: Gateone: Only import hosts if is the very first run.

--- librenms/gateone/files/entrypoint.sh
+++ librenms/gateone/files/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -e
 
 NO_DNS_HOSTS_FILE=/tmp/token/hosts.nodns
+LOCK_FILE=/var/lock/firsttime.lock
 
-# Import hosts that are not in DNS
-if [ -f "$NO_DNS_HOSTS_FILE" ]; then
-	cat ${NO_DNS_HOSTS_FILE} >> /etc/hosts
+# Only do this if it is the very first time
+if [ ! -f "$LOCK_FILE" ]; then
+	# Import hosts that are not in DNS
+    if [ -f "$NO_DNS_HOSTS_FILE" ]; then
+        cat ${NO_DNS_HOSTS_FILE} >> /etc/hosts
+    fi
+    touch "$LOCK_FILE"
 fi
 
 # Execute original gateone command
